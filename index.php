f9da7567a8092a3c9b713fce38745d21f729c16e
SuperChoque123 <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-11-17 08:04:44 -0300

    Add - index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Nos Trilhos</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../login.js">
+</head>
+
+<body>
+    <div class="incial1">
+        <div>
+            <h1>NOS TRILHOS</h1>
+
+        </div>
+        <div>
+            <div>
+                <p><b>Loading...</b></p>
+                <img src="../images/trilhos.png" alt="">
+            </div>
+            <a href="login.php">
+                <button class="btn-entrar">Entrar</button>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
