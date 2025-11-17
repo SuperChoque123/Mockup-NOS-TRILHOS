19156f2945d79c5e47d1577ab1cde8b721b597b6
SuperChoque123 <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-11-17 08:05:07 -0300

    Add - login

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nos Trilhos - Bem-vindo</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../style.css">
+  <link rel="stylesheet" media="(max-width: 800px)"/>
+  
+</head>
+<body>
+  <header>
+  <div class="topo1">
+    NOS TRILHOS
+  
+  </div>
+  </header>
+
+  <div class="container">
+    <div class="card">
+      <h1>BEM-<br>VINDO</h1>
+      <p>NOS TRILHOS aonde você tem acesso às linhas de trem.</p>
+      <a href="loginfaca.php" class="login-btn">
+        Login <i class=""></i>
+        <img src="../images/user (2).png" alt="">
+      </a>
+
+      
+    </div>
+  </div>
+</body>
+</html>
